4e32d7325f9a68655ef3ea0f7103e5a3ca07352a
Vincent Markert <vincentmarkert@gmx.de>
2020-10-30 12:36:18 +0100

    Removed unused code

--- src/false_joins.py
+++ src/false_joins.py
@@ -1,23 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-
 from scipy.spatial import Voronoi
-import cv2
+
 
 def voronoi_centers(num=1000):
     # sample a number of points in the unit cube
     samples = np.random.rand(num, 3)
     return samples
-
-
-def _adjust_bounds(ax, points):
-    # as in scipy.spatial._plotutils._adjust_bounds
-    margin = 0.1 * points.ptp(axis=0)
-    xy_min = points.min(axis=0) - margin
-    xy_max = points.max(axis=0) + margin
-    ax.set_xlim(xy_min[0], xy_max[0])
-    ax.set_ylim(xy_min[1], xy_max[1])
 
 
 def draw_collection_lines(verts):
@@ -30,6 +20,8 @@ def draw_collection_lines(verts):
             [verts[itr-1, 2], verts[itr, 2]],
             color="k"
         )
+    return
+
 
 centers = voronoi_centers(8)
 vor = Voronoi(centers)
@@ -79,16 +71,3 @@ ax.set_ylabel('y')
 ax.set_zlabel('z')
 
 plt.show()
-
-
-# points = np.random.rand(10,2) #random
-# from scipy.spatial import Voronoi, voronoi_plot_2d
-# vor = Voronoi(points)
-#
-# fig = voronoi_plot_2d(vor)
-#
-# fig = voronoi_plot_2d(vor, show_vertices=False, line_colors='orange',
-#                 line_width=2, line_alpha=0.6, point_size=2)
-# plt.show()
-#
-# cv2.imwrite("./test.jpg", fig)
